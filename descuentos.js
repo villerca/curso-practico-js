066e224baea57f9addd5c46b2be832ce521a99cb
villerca <villerca@gmail.com>
2022-04-02 17:34:41 -0300

    reto de calculo de descuentos con cupones

---
+++ descuentos.js
@@ -0,0 +1,42 @@
+//const  precioOriginal = 120;
+//const descuento = 18;
+
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+
+}
+
+
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultPrice = document.getElementById("resultPrice");
+    resultPrice.innerText = "El precio final con descuento a pagar son: $" + precioConDescuento;
+
+}
+
+
+
+
+
+
+
+
+
+/*console.log({
+    precioOriginal,
+    descuento,
+    porcentajePrecioconDescuento,
+    precioConDescuento
+});*/
